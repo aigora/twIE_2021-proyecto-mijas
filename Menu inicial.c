19e77a34d476f45ccbece6b129ede1cf96ebb843
DiegoFernandesAlves <80524709+DiegoFernandesAlves@users.noreply.github.com>
2021-04-08 16:48:46 +0200

    He cambiado las subopciones de cliente.

--- Menu inicial.c
+++ Menu inicial.c
@@ -12,39 +12,56 @@ printf("Conque identidad se relaciona:\n");
 printf("Cliente 'C',Proveedor 'P', Publicista 'Pu', Gobierno 'G':\t");
 scanf("%c",&bienvenida);
 
-switch(bienvenida) {
-	
+switch(bienvenida) 
+{
 	case 'C':
 	case 'c':
-		{ printf("Buenas cliente, escriba qué quiere consultar o realizar:\n");
-    printf("consultar la carta, 'carta';\n");
-    printf("calculadora de precios, 'calcular';\n");
-    printf("consultar suplementos, 'suplementos';\n");
-    printf("localización del restaurante, 'local';\n");
-    printf("informe de alergenos, 'alergia';\n");
-    scanf("%c",&seleccion11);
-    do
-    {
-      switch (seleccion11)
-    {
-    case 'carta':
-        printf("carta\n");
-        break;
-    case 'calcular':
-        printf("calculadora\n");
-        break;
-    case 'suplementos':
-        printf("suplementos\n");
-        break;
-    case 'local':
-        printf("localización\n");
-        break;
+		{
+        printf("Buenas cliente, escriba qué quiere consultar o realizar:\n");
+        printf("consultar la carta, 'CRT';\n");
+        printf("calculadora de precios, 'CLC';\n");
+        printf("consultar suplementos, 'SPL';\n");
+        printf("localización del restaurante, 'LCL';\n");
+        printf("informe de alergenos, 'ALRG'.");
+        do
+            {
+                scanf("%c",&seleccion11);
+                switch (seleccion11)
+                {
+                    case 'CRT':
+                        printf("carta\n");
+                        break;
+                    case 'CLC':
+                        printf("calculadora\n");
+                        break;
+                    case 'SPL':
+                        printf("suplementos\n");
+                        break;
+                    case 'LCL':
+                        printf("localización\n");
+                        break;
+                    case 'ALRG':
+                        printf("alergenos\n");
+                        break;
+                    default:
+                        printf("Escriba su seleccion tal y como se le indica.\n");
+                        break;
+                }
+            }
+        while ((seleccion11!='CRT') && (seleccion11!='CLC') && (seleccion11!='SPL') && (seleccion11!='LCL') && (seleccion11!='ALRG'));
+        printf ("\n");
+    }
+	break;
+        
     case 'alergia':
         printf("alergenos\n");
         break;
     default:
         printf("Escriba su seleccion tal y como se le indica.\n");
         break;
+    
+        
+    
     }
     }
     while (seleccion11!=carta && seleccion11!=calcular && seleccion11!=suplementos && seleccion11!=local && seleccion11!=alergia);
