b2f2c7bc3642067c822f29e4eb974cf670f2382f
DiegoBastida <d.bastida@alumnos.upm.es>
2021-04-08 12:36:21 +0200

    Update Menu inicial.c

--- Menu inicial.c
+++ Menu inicial.c
@@ -5,7 +5,7 @@ int main()
 
   
 char c;
-char bienvenida,carta,calcular,suplementos,local,alergia,seleccion11;
+char bienvenida;
 	
 printf("Buenos dias, bienvenido al restarante Mijas.\n");
 printf("Conque identidad se relaciona:\n");
@@ -16,40 +16,7 @@ switch(bienvenida) {
 	
 	case 'C':
 	case 'c':
-		{ printf("Buenas cliente, escriba qué quiere consultar o realizar:\n");
-    printf("consultar la carta, 'carta';\n");
-    printf("calculadora de precios, 'calcular';\n");
-    printf("consultar suplementos, 'suplementos';\n");
-    printf("localización del restaurante, 'local';\n");
-    printf("informe de alergenos, 'alergia';\n");
-    scanf("%c",&seleccion11);
-    do
-    {
-      switch (seleccion11)
-    {
-    case 'carta':
-        printf("carta\n");
-        break;
-    case 'calcular':
-        printf("calculadora\n");
-        break;
-    case 'suplementos':
-        printf("suplementos\n");
-        break;
-    case 'local':
-        printf("localización\n");
-        break;
-    case 'alergia':
-        printf("alergenos\n");
-        break;
-    default:
-        printf("Escriba su seleccion tal y como se le indica.\n");
-        break;
-    }
-    }
-    while (seleccion11!=carta && seleccion11!=calcular && seleccion11!=suplementos && seleccion11!=local && seleccion11!=alergia);
-    printf ("\n");
-		}
+	printf("Que desea:");
 	break;
 	
 	case 'P':
