a230c5baf3b42d07ff92767cb4f777709112f8b3
DiegoBastida <61050172+DiegoBastida@users.noreply.github.com>
2021-05-12 20:26:20 +0200

    Creación de librerias
    He agrupado las opciones pricipales en librerias, y su correspondiente implementacion(funciones)

--- Menu inicial.c
+++ Menu inicial.c
@@ -19,100 +19,12 @@ int main(){
 
       case 'C':
       case 'c':
-
-        do{
-        system ("cls");
-        printf("Bienvenido cliente, escriba que quiere consultar o realizar:\n");
-        printf("- Consultar la carta, 'C';\n");
-        printf("- Calculadora de precios, 'P';\n");
-        printf("- Consultar suplementos, 'S';\n");
-        printf("- Localizacion del restaurante, 'L';\n");
-        printf("- Informe de alergenos, 'A'\n\n");
-
-
-
-
-            scanf("%c", &op2);
-            printf("\n");
-
-            switch (op2)
-            {
-                case 'c':
-                case 'C':
-                    printf("carta\n");
-                    break;
-                case 'p':
-                case 'P':
-                    printf("calculadora\n");
-                    break;
-                case 'S':
-                case 's':
-                    printf("Suplementos\n");
-                    break;
-                case 'L':
-                case 'l':
-                    printf("Localizacion\n");
-                    break;
-                case 'A':
-                case 'a':
-                    printf("Alergenos\n");
-                    break;
-
-                default:
-
-                    break;
-            }
-
-
-        }while (op2!='c'  &&  op2!='p' &&  op2!='s' &&  op2!='l' &&  op2!='a' && op2!='C'  &&  op2!='P' &&  op2!='S' &&  op2!='L' &&  op2!='A');
-
-
-
+            cliente(op1);
           break;
 
       case 'P':
       case 'p':
-
-      do{
-
-
-        system("cls");
-        printf("Bienvenido Proveedor\n");
-        printf("Identificarse por tipo de producto: ");
-        printf("- Bebida, 'M'\n");
-        printf("- Comida, 'C'\n");
-        printf("- Tabaco, 'T'\n");
-        printf("- Otro, 'O'\n");
-
-
-            scanf("%c", &op3);
-
-            switch (op3)
-            {
-                case 'm':
-                case 'M':
-                    printf("Bebida\n");
-                    break;
-                case 'c':
-                case 'C':
-                    printf("Comida\n");
-                    break;
-                case 't':
-                case 'T':
-                    printf("Tabaco\n");
-                    break;
-                case 'o':
-                case 'O':
-                    printf("Otro\n");
-                    break;
-                default:
-
-                    break;
-            }
-
-
-      }while(op3!='m'  &&  op3!='M' &&  op3!='c' &&  op3!='C' &&  op3!='t' && op3!='T'  &&  op3!='O' &&  op3!='o');
-
+          proveedor(op1);
         break;
 
       case 'u':
@@ -122,35 +34,15 @@ int main(){
 
       case 'G':
       case 'g':
+          gobierno(op1);
 
-      do{
-
-
-        system("cls");
-        printf("¿Pertenece a Sanidad o Policia (Sanidad 'S', Policia 'P')?: ");
-        scanf("%c", &op4);
-        printf("\n");
-
-
-      }while(op4 !='P' && op4!= 'p' && op4!= 's' && op4!= 'S');
-       if(op4 == 's' || op4 == 'S'){
-            printf("Bienvenido Sanidad\n");
-        }else{
-            printf("Bienvenido Policia\n");
-        }
-      break;
-
-
-
-      default:
+break;
+        default:
           printf("\n");
-
-          break;
       }
 
     printf("\n");
     }while (op1!='C'  &&  op1!='U' &&  op1!='P' &&  op1!='G' &&  op1!='S' && op1!='c'  &&  op1!='u' &&  op1!='p' &&  op1!='g' &&  op1!='s');
-
 
     return 0;
 }
