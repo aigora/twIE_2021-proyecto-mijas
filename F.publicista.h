a0561a86fbf18ee1f91bf5a07463fbef1afd93e1
ignaciocartes99 <47812674+ignaciocartes99@users.noreply.github.com>
2021-05-27 11:30:43 +0200

    Update F.publicista.h

--- F.publicista.h
+++ F.publicista.h
@@ -1,12 +1,13 @@
+void identificar();
+void contratacion();
+void vercontrato();
+void nuevocontrato();
+void cambio(char aux[]);
+
 typedef struct{
     char agencia[20];
     char nombre[20];
     char apellido[20];
 
 }identificacion;
-
-void identificar();
-void contratacion();
-void vercontrato();
-void nuevocontrato();
 
