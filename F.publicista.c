430ee5beb93e516cccd28f4d76f2b20e25b642cd
ignaciocartes99 <47812674+ignaciocartes99@users.noreply.github.com>
2021-05-27 11:29:01 +0200

    Update F.publicista.c

--- F.publicista.c
+++ F.publicista.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 
 void identificar();
 void contratacion();
 void vercontrato();
 void nuevocontrato();
+void cambio(char aux[]);
 
 typedef struct{
     char agencia[20];
@@ -13,9 +16,11 @@ typedef struct{
 
 }identificacion;
 
-identificacion publi;
+
 
 int main(){
+
+
 
     printf("Dirigiendose a 'identificacion'\n");
 
@@ -32,6 +37,8 @@ int main(){
 
 void identificar(){
 
+     identificacion publi;
+
     printf("\033[2J");
 
     FILE *f;
@@ -39,7 +46,7 @@ void identificar(){
     f = fopen("identificacion_publicista.txt", "w");
 
     if (f == NULL){
-        printf("No se ha podido abrir.\n");
+        printf("identificacion_publicista.txt No se ha podido abrir.\n");
         exit(1);
     }
 
@@ -47,22 +54,28 @@ void identificar(){
     printf("Identifiquese: \n");
     printf("Introduzca su agencia: ");
     fgets(publi.agencia, 30, stdin);
+    fflush(stdin);
+
 
     fprintf(f, "Agencia: %s", publi.agencia);
-
+cambio(publi.agencia);
     printf("\n");
 
     printf("Introduzca su nombre: ");
     fgets(publi.nombre, 20, stdin);
-    fprintf(f, "Nombre: %s", publi.nombre);
+    fflush(stdin);
 
+    fprintf(f, "Nombre: %s", publi.nombre);
+ cambio(publi.nombre);
 
     printf("\n");
 
     printf("Introduzca su apellido: ");
     fgets(publi.apellido, 20, stdin);
-    fprintf(f, "Apellido: %s", publi.apellido);
+    fflush(stdin);
 
+    fprintf(f, "Apellido: %s", publi.apellido);
+ cambio(publi.apellido);
 
 
     fclose(f);
@@ -75,16 +88,22 @@ void identificar(){
 
 void contratacion(){
       char op1;
+       identificacion publi;
 
     do{
 
 
 
     printf("\033[2J");
-    printf("Bienvenido:\n");
-    printf("%s", publi.nombre);
-    printf("%s\n", publi.apellido);
-    printf("Selecione que acción quiere realizar\n");
+    printf("Bienvenido: ");
+
+    printf("%s ", publi.nombre);
+
+    printf("%s ", publi.apellido);
+
+    printf("de %s", publi.agencia);
+    printf("\n\n");
+    printf("Selecione que accion quiere realizar\n");
     printf("- Ver contrato existente 'E'\n");
     printf("- Realizar nuevo contrato 'N'\n");
     printf("- Salir\n");
@@ -120,61 +139,140 @@ void contratacion(){
 void vercontrato(){
 
     char aux[500];
+    char opcion;
 
     printf("VER CONTRATO EXISTENTE: \n\n");
 
     FILE *f;
 
-    f = fopen("contrato_existente.txt", "r");
+    f = fopen("contrato_publicista.txt", "r");
 
     if (f == NULL){
-        printf("No se ha podido abrir.\n");
-        exit(1);
+        printf("No hay ningun contrato aun.\n");
+     printf("Volviendo al menu inicial.");
+     sleep(2);
+     contratacion();
+     exit(1);
     }
 
     while(!feof(f)){
         fgets(aux, 500, f);
         printf("%s", aux);
     }
-    printf("\n");
-
-    fclose(f);
 
 
+ fclose(f);
 }
 
 void nuevocontrato(){
     char aux[500];
     char acepto;
+     identificacion contrato;
     printf("\033[2J");
 
     printf("NUEVO CONTRATO: \n\n");
 
-     FILE *f;
 
-    f = fopen("nuevo_contrato.txt", "r");
+FILE *f;
+
+    f = fopen("contrato_publicista.txt", "w");
 
     if (f == NULL){
         printf("No se ha podido abrir.\n");
         exit(1);
     }
+    fflush(stdin);
+    printf("Bienvenido publicista\n\n");
+    printf("Identifiquese: \n");
+    printf("Introduzca su agencia: ");
+    fgets(contrato.agencia, 10, stdin);
+    fflush(stdin);
+    cambio(contrato.agencia);
+     printf("\n");
 
-    while(!feof(f)){
+    printf("Introduzca su nombre: ");
+    fgets(contrato.nombre, 10, stdin);
+    fflush(stdin);
+   cambio(contrato.nombre);
+
+
+    printf("\n");
+
+    printf("Introduzca su apellido: ");
+    fgets(contrato.apellido, 10, stdin);
+    fflush(stdin);
+    cambio(contrato.apellido);
+
+
+    fprintf(f, "CONTRATO DE PUBLICIDAD - RESTAURANTE MIJAS\n");
+    fprintf(f, "-------------------------------------------\n");
+fprintf(f, "CONTRATO DE SERVICIOS DE PUBLICIDAD QUE CELEBRAN POR UNA PARTE EL RESTAURANTE MIJAS, REPRESENTADO EN ESTE ACTO POR LOS DUENOS DEL MISMO,\n");
+fprintf(f, "REPRESENTANTES LEGALES A QUIEN EN LO SUCESIVO Y PARA LOS EFECTOS DEL PRESENTE CONTRATO SE\n");
+fprintf(f, "LE DENOMINARA RESTAURANTE Y POR LA OTRA PARTE LA AGENCIA PUBLICITARIA _%s_\n", contrato.agencia);
+fprintf(f, "REPRESENTADA EN ESTE POR EL __%s_%s__ A QUIEN EN LO SUCESIVO Y PARA\n", contrato.nombre, contrato.apellido);
+fprintf(f, "EFECTOS DEL PRESENTE CONTRATO SE LE DENOMINARA PUBLICISTA, DE ACUERDO A LAS\n");
+fprintf(f, "SIGUIENTES DECLARACIONES Y CLAUSULAS:\n");
+fprintf(f, "DECLARACIONES\n");
+fprintf(f, "1. DECLARA __%s__ POR CONDUCTO DE SU REPRESENTANTE.\n", contrato.agencia);
+fprintf(f, "1.1. Que es una agencia de publicidad constituida conforme a las Leyes Españolas según consta\n");
+fprintf(f, "en lo dispuesto en la resolucion del Consejo General de Madrid,\n");
+fprintf(f, "donde se otorga el registro definitivo como __%s s.l.:_%s_%s__ expedido en su\n", contrato.agencia, contrato.nombre, contrato.apellido);
+fprintf(f, "sesion ordinaria.\n");
+fprintf(f, "2. DECLARA EL PUBLICISTA\n");
+fprintf(f, "2.1. Ser una Sociedad Civil constituida y validamente existente conforme a las Leyes Españolas.\n ");
+
+
+   fclose(f);
+  printf("\n");
+
+ f = fopen("contrato_publicista.txt", "r");
+
+    if (f == NULL){
+        printf("contrato_publicista.txt No se ha podido abrir.\n");
+        exit(1);
+    }
+
+
+while(!feof(f)){
         fgets(aux, 500, f);
         printf("%s", aux);
     }
 
-
     fclose(f);
 
-    printf("\n\n");
+printf("\n");
 
     printf("Acepta el nuevo contrato?: ('S': SI, 'N': NO) ");
     scanf("%c", &acepto);
 
-    if(acepto=='S' && acepto=='s'){
+    if(acepto=='s'||acepto=='S'){
         printf("Enhorabuena, bienvenido al Restaurant Mijas\n");
+        sleep(2);
+        printf("\033[2J");
+        printf("Volviendo al menu inicial.");
+     sleep(2);
+     contratacion();
     }else{
-        printf("Que lastima, esperemos que vuelva pronto. ");
+        printf("Que lastima, esperemos que vuelva pronto.\n");
+        printf("\033[2J");
+        printf("Volviendo al menu inicial.");
+        sleep(2);
+     contratacion();
     }
+
+
+
+
 }
+
+void cambio(char aux[]){
+    int i;
+
+    for(i=0;i<50;i++){
+        if(aux[i] == '\n'){
+            aux[i] = '\0';
+        }
+    }
+
+}
+
