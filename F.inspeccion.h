7a9ae8009712e7fbf9b7d2d47d5fc13185ac4bdb
ignaciocartes99 <47812674+ignaciocartes99@users.noreply.github.com>
2021-07-14 00:47:18 +0200

    Delete F.inspeccion.h

--- F.inspeccion.h
+++
@@ -1,7 +0,0 @@
-#include "F.cliente.h"
-#include "cabeceras.h"
-
-int compContrato(char nombre,int contra);
-int compSalario(char nombre,char tipo[],float saldo);
-int compVacaciones(char nombre, char tipo[],int vacas);
-
